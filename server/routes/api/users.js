1be7a5dc9a02f87b5209adf1299eb4fa1cd7a812
Erik Nuber <erik.nuber@yahoo.com>
2021-02-02 17:24:07 -0800

    added ability to update email

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -57,41 +57,69 @@ router.route("/signin")
     })
 
 router.route("/profile")
-.get(checkLoggedIn,grantAccess('readOwn','profile'),async (req,res)=>{
-    try {
-        const permission = res.locals.permission;
-        const user = await User.findById(req.user._id);
-        if(!user) return res.status(400).json({message:'User not found'});
+    .get(checkLoggedIn,grantAccess('readOwn','profile'),async (req,res)=>{
+        try {
+            const permission = res.locals.permission;
+            const user = await User.findById(req.user._id);
+            if(!user) return res.status(400).json({message:'User not found'});
 
-        res.status(200).json(permission.filter(user._doc));
-    }catch(error){
-        return res.status(400).send(error);
-    }
-})
-.patch(checkLoggedIn, grantAccess('updateOwn', 'profile'), async (req, res)=>{
-    try {
-        const user = await User.findOneAndUpdate(
-            {_id: req.user._id},
-            {
-                "$set": {
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    age: req.body.age
-                }
-            },
-            { new: true}
-        );
-        if (!user) return res.status(400).json({message: 'User not found'});
-        res.status(200).json(getUserProps(user));
-    } catch (error) {
-        res.status(400).json({message: "Problem updating", error: error})
-    }
-});
+            res.status(200).json(permission.filter(user._doc));
+        }catch(error){
+            return res.status(400).send(error);
+        }
+    })
+    .patch(checkLoggedIn,grantAccess('updateOwn','profile'),async (req,res)=>{
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.user._id},
+                {
+                    "$set":{
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        age: req.body.age
+                    }
+                },
+                { new: true }
+            );
+            if(!user) return res.status(400).json({message:'User not found'})
+
+            res.status(200).json(getUserProps(user))
+        } catch(error){
+            res.status(400).json({message:"Problem updating",error:error});
+        }
+    });
 
 router.route('/isauth')
 .get(checkLoggedIn, async(req, res)=>{
   res.status(200).send(getUserProps((req.user)));
 });
+
+router.route("/update_email")
+    .patch(checkLoggedIn, grantAccess('updateOwn','profile'),async (req,res)=>{
+        try{
+            /// make
+            if(await User.emailTaken(req.body.newemail)){
+                return res.status(400).json({message:"Sorry email taken"})
+            }
+
+            const user = await User.findOneAndUpdate(
+                {_id: req.user._id, email: req.body.email },
+                {
+                    "$set":{
+                        email: req.body.newemail
+                    }
+                },
+                {new:true}
+            );
+            if(!user) return res.status(400).json({message:'User not found'})
+
+            const token = user.generateToken();
+            res.cookie('x-access-token',token)
+                .status(200).send({email:user.email})
+        } catch(error){
+            res.status(400).json({message:"Problem updating",error:error});
+        }
+    });
 
 
 const getUserProps = (user) => {
@@ -103,7 +131,7 @@ const getUserProps = (user) => {
         age: user.age,
         role: user.role
     }
-}
+};
 
 
 module.exports = router;
