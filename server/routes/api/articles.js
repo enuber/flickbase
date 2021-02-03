7e6e7d8c803475270a2248e377dc89d26ff777d9
Erik Nuber <erik.nuber@yahoo.com>
2021-02-02 18:09:21 -0800

    created the model for articles

---
+++ server/routes/api/articles.js
@@ -0,0 +1,22 @@
+const express  = require('express');
+let router = express.Router();
+const { checkLoggedIn } = require('../../middleware/auth');
+const { grantAccess } = require('../../middleware/roles');
+
+// model
+const { Article } = require('../../models/article_model');
+
+// add single article
+// admin get,patch,delete single article (draft or public)
+// get articles no auth
+// fetch articles load more
+// fetch articles, with pagination
+
+
+
+
+
+
+
+
+module.exports = router;
